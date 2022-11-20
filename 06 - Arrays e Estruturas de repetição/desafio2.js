b963c58177d89fd3c5e8aa0b01fc44fd660a6a2d
CrisCaxile <cristovao.caxile1@gmail.com>
2022-11-20 15:53:11 -0300

    update desafio2

--- 06 - Arrays e Estruturas de repetição/desafio2.js
+++ 06 - Arrays e Estruturas de repetição/desafio2.js
@@ -15,3 +15,13 @@ function verificarNumerosPares() {
 
 
 verificarNumerosPares();
+
+let lista = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+w = 0;
+while (w < lista.length) {
+    if (lista[w] % 2 === 0 ){
+    console.log('Os numeros pares foram:',lista[w]);
+    }
+    w++;
+}
+
